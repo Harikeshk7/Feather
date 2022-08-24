7c005b5467564ec155c53436be1d6cf5384b3aeb
Harikeshk7 <57568361+Harikeshk7@users.noreply.github.com>
2022-08-23 21:08:39 -0400

    Add files via upload

---
+++ amp_arduino_lora_sender.ino
@@ -0,0 +1,106 @@
+// LoRa 9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example LoRa9x_RX
+ 
+#include <SPI.h>
+#include <RH_RF95.h>
+ 
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 2
+
+#define KILL_BUTTON 5
+ 
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+ 
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Variables will change:
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+char killpacket[10] = "Kill"; 
+char idlepacket[10] = "Idle";
+
+void setup() 
+{
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+ 
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  pinMode(KILL_BUTTON, INPUT);
+ 
+  delay(100);
+ 
+  while (!rf95.init()) {
+    while (1);
+  }
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    while (1);
+  }
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+ 
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+ 
+void loop()
+{
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(KILL_BUTTON);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+ 
+          rf95.send((uint8_t *)killpacket, 5);
+          
+          delay(10);
+          rf95.waitPacketSent();
+      }
+    }
+  }
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  rf95.send((uint8_t *)idlepacket, 5);
+  
+  lastButtonState = reading;
+  delay(50);
+}
